8052c6cf2d08537cea3e1c89e3ce32beb2d0ce88
= <=>
2022-08-14 13:36:20 +0300

    Lists and Keys

--- tech-hub/src/Content.js
+++ tech-hub/src/Content.js
@@ -21,29 +21,49 @@ function Content() {
     }
   ])
   const handleCheck = id => {
-    console.log(`Key: ${id}`)
+    const listItems = items.map((item) => item.id === id ? { ...item, checked: !item.checked } : item)
+    setItems(listItems)
+    localStorage.setItem('shopping-list', JSON.stringify(listItems));
   }
   
+  // delete item
+  const handleDelete = id => {
+    const listItems = items.filter(item => item.id !== id);
+    setItems(listItems);
+    localStorage.setItem('shopping-list', JSON.stringify(listItems));
+  }
   return (
     <main>
-      <ul>
-       {
-        items.map(item => (
-          <li className='item' key={ item.id }>
-            <input 
-            type="checkbox"
-            onChange={() => handleCheck(item.id)}
-            checked= { item.checked }
-            />
-            <label>{ item.description }</label>
-            <FaTrashAlt
-             role="button" 
-             tabIndex="2"
-             />
-          </li>
-        ))
-       }
-      </ul>
+      { items.length ? (
+        <ul>
+        {
+          items.map(item => (
+            <li className='item' key={ item.id }>
+              <input 
+              type="checkbox"
+              onChange={() => handleCheck(item.id)}
+              checked= { item.checked }
+              />
+              <label
+              style={ item.checked ? {
+                textDecoration: 'line-through'
+              } : null}
+                onDoubleClick={ () => handleCheck(item.id) }
+              >{ item.description }</label>
+              <FaTrashAlt
+              onClick={ () => handleDelete(item.id)}
+              role="button" 
+              tabIndex="2"
+              />
+            </li>
+          ))
+        }
+        </ul>
+      ) : (
+        <p  style={{
+          marginTop: '2rem'
+        }}>Your list is empty.</p>
+      )}
     </main>
   )
 }
